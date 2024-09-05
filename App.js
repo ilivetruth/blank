2ceedca118985019335b82b9c3731e0688172c00
Earl <ijustlivetruth@gmail.com>
2024-09-05 00:12:25 -0400

    "herei s the message"

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, useColorScheme, useTheme } from "react-native";
+import { Provider as PaperProvider } from "react-native-paper";
+import { LightCustomTheme, DarkCustomTheme } from "./components/theme";
 
 export default function App() {
+  const colorScheme = useColorScheme();
+
+  const theme = colorScheme === "dark" ? DarkCustomTheme : LightCustomTheme;
+  const { colors } = useTheme();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider theme={theme}>
+      <View
+        style={[styles.container, { backgroundColor: colors.backgroundColor }]}
+      >
+        <Text style={{ color: colors.text }}>
+          Open up App.js to start working on your app!
+        </Text>
+        <StatusBar style="auto" />
+      </View>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
